83e7878c3d506bb01a3095ae07f37ecc78568a31
Manisha884 <85836219+manisha884@users.noreply.github.com>
2021-06-21 10:22:19 +0530

    First Assignment
    Different types of c++ program

---
+++ sumoftwonumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+cout << "Hello world!" << endl;
+    int x,y, sum;
+    cout <<"Please enter the value of x : ";
+    cin >> x; //1
+
+    cout << "Please enter the value of  y: ";
+    cin >> y;
+    sum = x+y;  //2
+    cout << endl << "sum of two number : "<<sum;
+
+
+return 0;
+}
---
+++ sumoftwonumber1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+cout << "Hello world!" << endl;
+    int x=29;
+    int y=30;
+    int sum=x+y;
+
+    cout << endl << "sum of two number : "<<sum;
+
+return 0;
+}
