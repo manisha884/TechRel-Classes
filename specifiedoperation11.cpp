ddfdd1d10ca3aa3cf03196f048a7bd4a54741d78
Manisha884 <85836219+manisha884@users.noreply.github.com>
2021-06-24 23:08:31 +0530

    Write a program in C++ to print the result of the specified operations
     Write a program in C++ to print the result of the specified operations

---
+++ specifiedoperation11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n\n Print the result of some specific operation :\n";
+	cout << "--------------------------------------------------\n";
+    cout << " Result of 1st expression is : "<< (1+7*5) <<"\n" ;
+    cout << " Result of 2nd expression is : "<< ((24+5)%2) <<"\n" ;
+    cout << " Result of 3rd expression is : "<< (10+4*3/6) <<"\n" ;
+    cout << " Result of 4th expression is : "<< (2+12/4*5-9%4) <<"\n\n" ;
+return 0;
+}
